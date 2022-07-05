5c30292ca2a98deec1cd581ae0783e1bb53154cf
JIM SINATRA <jimsinatra30@gmail.com>
2022-07-05 16:34:25 +0200

    test blade

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>How to send mail using queue in Laravel 9? - ItSolutionStuff.com</title>
+    <title>Merhaba</title>
 </head>
 <body>
    
 <center>
 <h2 style="padding: 23px;background: #b3deb8a1;border-bottom: 6px green solid;">
-    <a href="https://itsolutionstuff.com">Visit Our Website : ItSolutionStuff.com</a>
+    <a href="#">Merhaba</a>
 </h2>
 </center>
   
